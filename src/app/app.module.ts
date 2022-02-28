68d07f3e67c290d2db5214def91b7574fb8c191e
Büşra Özata <busraozata@Busra-MacBook-Air.local>
2022-02-28 03:34:34 +0300

    Navbar, table, information added and revized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataWareHouseComponent } from './components/data-ware-house/data-ware-house.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/data-ware-house/data-ware-house.component.ts
+++ src/app/components/data-ware-house/data-ware-house.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormArray,
+  AbstractControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-data-ware-house',
   templateUrl: './data-ware-house.component.html',
-  styleUrls: ['./data-ware-house.component.scss']
+  styleUrls: ['./data-ware-house.component.scss'],
 })
 export class DataWareHouseComponent implements OnInit {
-
   active: any;
   subMenuToggle: any;
-
+  showData:any;
+  filterForm: FormGroup;
+ 
   dataWare: any = [
     {
       date: '2022-02-22',
@@ -84,12 +91,21 @@ export class DataWareHouseComponent implements OnInit {
       actions: '',
     },
   ];
-  constructor() {}
+  
+  constructor(private fb: FormBuilder) {
+    this.filterForm = this.fb.group({
+      filters: this.fb.array([this.fb.control(null)]),
+    });
+  }
 
   ngOnInit(): void {}
 
   showActive() {
-    this.active =!this.active;
+    this.active = !this.active;
+  }
+
+  isShowData(){
+    this.showData = !this.showData;
   }
 
   toggleMenu(event: any) {
@@ -100,4 +116,17 @@ export class DataWareHouseComponent implements OnInit {
     }
   }
 
+  total = 0;
+  addFilter(): void {
+    if (this.total < 3) {
+      (this.filterForm.get('filters') as FormArray).push(this.fb.control(null));
+      this.total++;
+    } else {
+      console.log('Stop');
+    }
+  }
+
+  getFiltersFormControls(): AbstractControl[] {
+    return (<FormArray>this.filterForm.get('filters')).controls;
+  }
 }
